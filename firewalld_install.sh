be7d5cf76df9a71b6a6925ec81a9fce0e9fd927c
weiguangchao <3409885610@qq.com>
2020-02-28 08:43:38 +0800

    Linxu_script

--- firewalld_install.sh
+++ firewalld_install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 yum install -y firewalld
-systemctl enable firewalld
-systemctl start firewalld
 systemctl disable iptables
 systemctl stop iptables
 yum remove iptables -y
+systemctl enable firewalld
+systemctl start firewalld
+systemctl status firewalld
